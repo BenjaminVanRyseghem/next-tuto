23f6ab15092056cfe077133e5c9c87a2b7edf476
Benjamin Van Ryseghem <benjamin.vanryseghem@gmail.com>
2024-05-29 12:40:11 +0200

    DONE

--- app/dashboard/customers/page.tsx
+++ app/dashboard/customers/page.tsx
@@ -1,3 +1,21 @@
-export default function Page() {
-    return <p>Customers Page</p>;
+import Table from '@/app/ui/customers/table';
+import {fetchFilteredCustomers} from '@/app/lib/data';
+import {Metadata} from 'next';
+
+export const metadata: Metadata = {
+    title: 'Customers',
+};
+
+export default async function Page({
+    searchParams,
+}: {
+    searchParams?: {
+        query?: string;
+        page?: string;
+    };
+}) {
+    const customers = await fetchFilteredCustomers(searchParams?.query ?? "");
+    return (
+        <Table customers={customers}/>
+    );
 }
